bec1cdf8f7c4087fb8f3ad831ba375178c22fcdf
kinneh <henrik-andreasson@users.noreply.github.com>
2008-11-19 10:17:52 +0000

    clean some not used config

--- syscheck/related-available/914_compare_master_slave_db.sh
+++ syscheck/related-available/914_compare_master_slave_db.sh
@@ -34,15 +34,15 @@ fi
 
 COMMAND1=`echo "show tables" | mysql -u $DB_USER -h $HOSTNAME_NODE1 --password=$DB_PASSWORD $DB_NAME | grep -v Tables_in_ejbca`
 for table in $COMMAND1; do 
-	echo "$table" >> $HOSTNAME_NODE1.txt
-	echo "select count(*) from $table" | mysql -u $DB_USER -h $HOSTNAME_NODE1 --password=$DB_PASSWORD $DB_NAME | grep -v 'count(\*)' >> $HOSTNAME_NODE1.txt
+	echo "$table" >> ${HOSTNAME_NODE1}.txt
+	echo "select count(*) from $table" | mysql -u $DB_USER -h $HOSTNAME_NODE1 --password=$DB_PASSWORD $DB_NAME | grep -v 'count(\*)' >> ${HOSTNAME_NODE1}.txt
 done
 
 COMMAND2=`echo "show tables" | mysql -u $DB_USER -h $HOSTNAME_NODE2 --password=$DB_PASSWORD $DB_NAME | grep -v Tables_in_ejbca`
 for table in $COMMAND2; do 
-	echo "$table" >> $HOSTNAME_NODE2.txt
-	echo "select count(*) from $table" | mysql -u $DB_USER -h $HOSTNAME_NODE2 --password=$DB_PASSWORD $DB_NAME | grep -v 'count(\*)' >> $HOSTNAME_NODE2.txt
+	echo "$table" >> ${HOSTNAME_NODE2}.txt
+	echo "select count(*) from $table" | mysql -u $DB_USER -h $HOSTNAME_NODE2 --password=$DB_PASSWORD $DB_NAME | grep -v 'count(\*)' >> ${HOSTNAME_NODE2}.txt
 done
 
-diff --side-by-side --width=75 $HOSTNAME_NODE1.txt $HOSTNAME_NODE2.txt
-rm $HOSTNAME_NODE1.txt $HOSTNAME_NODE2.txt
+diff --side-by-side --width=75 ${HOSTNAME_NODE1}.txt ${HOSTNAME_NODE2}.txt
+rm ${HOSTNAME_NODE1}.txt ${HOSTNAME_NODE2}.txt
