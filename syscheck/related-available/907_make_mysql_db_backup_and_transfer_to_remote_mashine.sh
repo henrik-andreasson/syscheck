9fd8740a8730f0ffc570a4798c6c8b22036c15e4
kinneh <henrik-andreasson@users.noreply.github.com>
2008-12-03 14:33:58 +0000

    add support for directorys to backup to

--- syscheck/related-available/907_make_mysql_db_backup_and_transfer_to_remote_mashine.sh
+++ syscheck/related-available/907_make_mysql_db_backup_and_transfer_to_remote_mashine.sh
@@ -43,7 +43,7 @@ if [ $? -ne 0 ] ;   then
 fi  
 
 for (( i = 0 ;  i < "${#BACKUP_HOST[@]}" ; i++ )) ; do
-	$SYSCHECK_HOME/related-enabled/906_ssh-copy-to-remote-machine.sh ${MYSQLBACKUPFULLFILENAME}.gz ${BACKUP_HOST[$i]}
+	$SYSCHECK_HOME/related-enabled/906_ssh-copy-to-remote-machine.sh ${MYSQLBACKUPFULLFILENAME}.gz ${BACKUP_HOST[$i]} ${BACKUP_DIR[$i]}
 	if [ $? -eq 0 ] ; then
 		printlogmess $INFO $BAK_ERRNO_1 "$BAK_DESCR_1"
 	else
