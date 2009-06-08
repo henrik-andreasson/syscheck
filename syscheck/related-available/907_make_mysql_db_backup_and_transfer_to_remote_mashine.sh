963dc3741c2d508c5046a9ce705289c381be6774
kinneh <henrik-andreasson@users.noreply.github.com>
2009-06-08 14:34:48 +0000

    ECA-1301 Mysql backup script should support putting backups in weekly|monthly and so on to support different schemes for saving backups

--- syscheck/related-available/907_make_mysql_db_backup_and_transfer_to_remote_mashine.sh
+++ syscheck/related-available/907_make_mysql_db_backup_and_transfer_to_remote_mashine.sh
@@ -17,21 +17,38 @@ getlangfiles $SCRIPTID
 getconfig $SCRIPTID
 
 PRINTTOSCREEN=
-if [ "x$1" = "x-h" -o "x$1" = "x--help" ] ; then
+
+schelp () {
 	echo "$BAK_HELP"
-	echo "$ERRNO_1/$BAK_DESCR_1 - $BAK_HELP_1"
-	echo "$ERRNO_2/$BAK_DESCR_2 - $BAK_HELP_2"
-	echo "$ERRNO_3/$BAK_DESCR_3 - $BAK_HELP_3"
-	echo "$ERRNO_4/$BAK_DESCR_4 - $BAK_HELP_4"
+	echo "$BAK_ERRNO_1/$BAK_DESCR_1 - $BAK_HELP_1"
+	echo "$BAK_ERRNO_2/$BAK_DESCR_2 - $BAK_HELP_2"
+	echo "$BAK_ERRNO_3/$BAK_DESCR_3 - $BAK_HELP_3"
+	echo "$BAK_ERRNO_4/$BAK_DESCR_4 - $BAK_HELP_4"
 	echo "${SCREEN_HELP}"
 	exit
-elif [ "x$1" = "x-s" -o  "x$1" = "x--screen" -o \
-    "x$2" = "x-s" -o  "x$2" = "x--screen"   ] ; then
-    PRINTTOSCREEN=1
-    shift
-fi 
+}
 
-$SYSCHECK_HOME/related-available/904_make_mysql_db_backup.sh -s
+
+
+TEMP=`/usr/bin/getopt --options "hsxdwmy" --long "help,screen,default,daily,weekly,monthly,yearly" -- "$@"`
+if [ $? != 0 ] ; then schelp ; fi
+echo "TEMP: >$TEMP<"
+eval set -- "$TEMP"
+
+while true; do
+  case "$1" in
+    -s|--screen  ) PRINTTOSCREEN=1; shift;;
+    -x|--default ) BACKUPARG=$1; shift;;
+    -d|--daily   ) BACKUPARG=$1; shift;;
+    -w|--weekly  ) BACKUPARG=$1; shift;;
+    -m|--monthly ) BACKUPARG=$1; shift;;
+    -y|--yearly  ) BACKUPARG=$1; shift;;
+    -h|--help )   schelp;shift;;
+    --) break;;
+  esac
+done
+
+FULLFILENAME=`$SYSCHECK_HOME/related-available/904_make_mysql_db_backup.sh -s ${BACKUPARG}`
 
 if [ $? -ne 0 ] ; then
     printlogmess $ERROR $BAK_ERRNO_2 "$BAK_DESCR_2"
@@ -42,7 +59,7 @@ if [ $? -ne 0 ] ;   then
 fi  
 
 for (( i = 0 ;  i < "${#BACKUP_HOST[@]}" ; i++ )) ; do
-	$SYSCHECK_HOME/related-enabled/906_ssh-copy-to-remote-machine.sh ${MYSQLBACKUPFULLFILENAME}.gz ${BACKUP_HOST[$i]} ${BACKUP_DIR[$i]} ${BACKUP_USER[$i]} 
+	$SYSCHECK_HOME/related-enabled/906_ssh-copy-to-remote-machine.sh ${FULLFILENAME} ${BACKUP_HOST[$i]} ${BACKUP_DIR[$i]} ${BACKUP_USER[$i]} 
 	if [ $? -eq 0 ] ; then
 		printlogmess $INFO $BAK_ERRNO_1 "$BAK_DESCR_1"
 	else
