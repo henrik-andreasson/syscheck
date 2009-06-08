5b09c772ded3f84c0eb9326dfecc673cb3676896
kinneh <kinneh@b4c053c8-dc63-40c4-9c96-5a9ce7d203c6>
2009-06-08 14:34:48 +0000

    ECA-1301 Mysql backup script should support putting backups in weekly|monthly and so on to support different schemes for saving backups

--- trunk/syscheck/related-available/904_make_mysql_db_backup.sh
+++ trunk/syscheck/related-available/904_make_mysql_db_backup.sh
@@ -14,31 +14,62 @@ getconfig $SCRIPTID
 ERRNO_1="${SCRIPTID}1"
 ERRNO_2="${SCRIPTID}2"
 ERRNO_3="${SCRIPTID}3"
+ERRNO_4="${SCRIPTID}4"
 
 PRINTTOSCREEN=0
-if [ "x$1" = "x-h" -o "x$1" = "x--help" ] ; then
-	echo "$ECRT_HELP"
+
+schelp () {
+	echo "$HELP"
 	echo "$ERRNO_1/$MYSQL_BACKUP_DESCR_1 - $MYSQL_BACKUP_HELP_1"
 	echo "$ERRNO_2/$MYSQL_BACKUP_DESCR_2 - $MYSQL_BACKUP_HELP_2"
 	echo "$ERRNO_3/$MYSQL_BACKUP_DESCR_3 - $MYSQL_BACKUP_HELP_3"
 	echo "${SCREEN_HELP}"
 	exit
-elif [ "x$1" = "x-s" -o  "x$1" = "x--screen" -o \
-    "x$2" = "x-s" -o  "x$2" = "x--screen"   ] ; then
-    PRINTTOSCREEN=1
+}
+
+
+TEMP=`/usr/bin/getopt --options "hsymwdx" --long "help,screen,default,daily,weekly,monthly,yearly" -- "$@"`
+if [ $? != 0 ] ; then help ; fi
+eval set -- "$TEMP"
+
+while true; do
+  case "$1" in
+    -x|--default ) TYPE=${SUBDIR_DEFAULT}; shift;;
+    -d|--daily   ) TYPE=${SUBDIR_DAILY}; shift;;
+    -w|--weekly  ) TYPE=${SUBDIR_WEEKLY}; shift;;
+    -m|--monthly ) TYPE=${SUBDIR_MONTHLY}; shift;;
+    -y|--yearly  ) TYPE=${SUBDIR_YEARLY}; shift;;
+    -s|--screen ) PRINTTOSCREEN=1; shift;;
+    -h|--help )   help;shift;;
+    --) break ;;
+  esac
+done
+
+EXTRADIR=
+if [ "x${TYPE}" = "x" ] ; then
+	EXTRADIR=${SUBDIR_DEFAULT}	
+else
+	EXTRADIR=${TYPE}
 fi
 
-$MYSQLDUMP_BIN -u root --password="$MYSQLROOT_PASSWORD" ejbca  > $MYSQLBACKUPFULLFILENAME 
+if [ ! -d "${MYSQLBACKUPDIR}/${EXTRADIR}" ] ; then
+	printlogmess $ERROR $ERRNO_3 "$MYSQL_BACKUP_DESCR_3" "${MYSQLBACKUPDIR}/${EXTRADIR}"
+	exit 1
+fi
+
+MYSQLBACKUPFULLFILENAME="${MYSQLBACKUPDIR}/${EXTRADIR}/${MYSQLBACKUPFILE}"
+dumpret=$($MYSQLDUMP_BIN -u root --password="${MYSQLROOT_PASSWORD}" ${DB_NAME} 2>&1 > ${MYSQLBACKUPFULLFILENAME} )
 
 if [ $? = 0 ] ; then
   gzip $MYSQLBACKUPFULLFILENAME
   if [ $? = 0 ] ; then
       printlogmess $INFO $ERRNO_1 "$MYSQL_BACKUP_DESCR_1" $MYSQLBACKUPFULLFILENAME.gz
+      echo "$MYSQLBACKUPFULLFILENAME.gz"
   else
       printlogmess $ERROR $ERRNO_2 "$MYSQL_BACKUP_DESCR_2" $MYSQLBACKUPFULLFILENAME
   fi  
 else
-  printlogmess $ERROR $ERRNO_2 "$MYSQL_BACKUP_DESCR_2" $MYSQLBACKUPFULLFILENAME
+  printlogmess $ERROR $ERRNO_4 "$MYSQL_BACKUP_DESCR_4" "$dumpret"
 fi 
 
 
