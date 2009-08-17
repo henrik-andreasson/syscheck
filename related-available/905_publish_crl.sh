1c88ba428c3f3a6e17f01ec2f267c18de089e443
kinneh <henrik-andreasson@users.noreply.github.com>
2009-08-17 12:49:36 +0000

    rename tag to 1.5.4

--- related-available/905_publish_crl.sh
+++ related-available/905_publish_crl.sh
@@ -62,13 +62,13 @@ get () {
 put () {
 
 	CRLHOST=$1
-        CRLFILE=$2
+        CRLNAME=$2
 	SSHSERVER_DIR=$3
 	SSHKEY=$4
 	SSHUSER=$5
 
         cd $CRLDIRECTORY
-	$SYSCHECK_HOME/related-enabled/906_ssh-copy-to-remote-machine.sh -s $CRLFILE $CRLHOST $SSHSERVER_DIR $SSHUSER $SSHKEY
+	$SYSCHECK_HOME/related-enabled/906_ssh-copy-to-remote-machine.sh -s $CRLNAME $CRLHOST $SSHSERVER_DIR $SSHUSER
 	if [ $? != 0 ] ; then
                 printlogmess $ERROR $ERRNO_2 "$PUBL_DESCR_2" $CRLHOST $CRLNAME
 	fi
