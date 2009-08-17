881b234df487b6266f2acf649b07be75d6a8bc3d
kinneh <henrik-andreasson@users.noreply.github.com>
2009-08-17 13:04:14 +0000

    tag 1.5.0

---
+++ related-available/904_make_mysql_db_backup.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Set default home if not already set.
+SYSCHECK_HOME=${SYSCHECK_HOME:-"/usr/local/syscheck"}
+
+# Import common resources
+. $SYSCHECK_HOME/resources.sh
+
+SCRIPTID=904
+
+getlangfiles $SCRIPTID 
+getconfig $SCRIPTID
+
+ERRNO_1="${SCRIPTID}1"
+ERRNO_2="${SCRIPTID}2"
+ERRNO_3="${SCRIPTID}3"
+
+PRINTTOSCREEN=0
+if [ "x$1" = "x-h" -o "x$1" = "x--help" ] ; then
+	echo "$ECRT_HELP"
+	echo "$ERRNO_1/$MYSQL_BACKUP_DESCR_1 - $MYSQL_BACKUP_HELP_1"
+	echo "$ERRNO_2/$MYSQL_BACKUP_DESCR_2 - $MYSQL_BACKUP_HELP_2"
+	echo "$ERRNO_3/$MYSQL_BACKUP_DESCR_3 - $MYSQL_BACKUP_HELP_3"
+	echo "${SCREEN_HELP}"
+	exit
+elif [ "x$1" = "x-s" -o  "x$1" = "x--screen" -o \
+    "x$2" = "x-s" -o  "x$2" = "x--screen"   ] ; then
+    PRINTTOSCREEN=1
+fi
+
+$MYSQLDUMP_BIN -u root --password="$MYSQLROOT_PASSWORD" ejbca  > $MYSQLBACKUPFULLFILENAME 
+
+if [ $? = 0 ] ; then
+  gzip $MYSQLBACKUPFULLFILENAME
+  if [ $? = 0 ] ; then
+      printlogmess $INFO $ERRNO_1 "$MYSQL_BACKUP_DESCR_1" $MYSQLBACKUPFULLFILENAME.gz
+  else
+      printlogmess $ERROR $ERRNO_2 "$MYSQL_BACKUP_DESCR_2" $MYSQLBACKUPFULLFILENAME
+  fi  
+else
+  printlogmess $ERROR $ERRNO_2 "$MYSQL_BACKUP_DESCR_2" $MYSQLBACKUPFULLFILENAME
+fi 
+
+
+
+
+
+
