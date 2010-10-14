e4271390d0e02fc2b7752d1f26acabfcf2cf5b13
kinneh <kinneh@b4c053c8-dc63-40c4-9c96-5a9ce7d203c6>
2010-10-14 06:54:09 +0000

    remove old clusterscritps

--- trunk/syscheck/misc/clusterscripts/createPriviledgesNode2.sql
+++
@@ -1,3 +0,0 @@
-GRANT ALL ON ejbca.* to 'ejbca'@'147.186.2.226' IDENTIFIED BY 'foo123';
-GRANT ALL ON ejbca.* to 'ejbca'@'147.186.2.228' IDENTIFIED BY 'foo123';
-GRANT ALL ON ejbca.* to 'ejbca'@'147.186.2.201' IDENTIFIED BY 'foo123';
