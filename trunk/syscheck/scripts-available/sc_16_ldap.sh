f0c52977142a4b5e5b9d982655f9566c82525c8d
kinneh <kinneh@b4c053c8-dc63-40c4-9c96-5a9ce7d203c6>
2010-10-14 07:42:40 +0000

    Closes #37 	add better support for changing the location of syscheck install

--- trunk/syscheck/scripts-available/sc_16_ldap.sh
+++ trunk/syscheck/scripts-available/sc_16_ldap.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 # Script that checks if the OpenLDAP ldap server is running.
 
+# Set SYSCHECK_HOME if not already set.
 
-# Set default home if not already set.
-SYSCHECK_HOME=${SYSCHECK_HOME:-"/usr/local/syscheck"}
+# 1. First check if SYSCHECK_HOME is set then use that
+if [ "x${SYSCHECK_HOME}" = "x" ] ; then
+# 2. Check if /etc/syscheck.conf exists then source that (put SYSCHECK_HOME=/path/to/syscheck in ther)
+    if [ -e /etc/syscheck.conf ] ; then 
+	source /etc/syscheck.conf 
+    else
+# 3. last resort use default path
+	SYSCHECK_HOME="/usr/local/syscheck"
+    fi
+fi
+
+if [ ! -f ${SYSCHECK_HOME}/syscheck.sh ] ; then echo "$0: Can't find syscheck.sh in SYSCHECK_HOME ($SYSCHECK_HOME)" ;exit ; fi
+
+
+
+
 
 # uniq ID of script (please use in the name of this file also for convinice for finding next availavle number)
 SCRIPTID=16
