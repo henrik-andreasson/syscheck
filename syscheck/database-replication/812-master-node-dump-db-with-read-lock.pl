f3d2214d9dd3a85942372f2372a7feecde131777
kinneh <henrik-andreasson@users.noreply.github.com>
2012-09-13 08:18:02 +0000

    svn updates

--- syscheck/database-replication/812-master-node-dump-db-with-read-lock.pl
+++ syscheck/database-replication/812-master-node-dump-db-with-read-lock.pl
@@ -2,6 +2,22 @@
 
 use DBI;
 
+print "Will in once sweep do the following:\n";
+print "Rest master on this host\n";
+print "lock tables during a backup\n";
+print "start the backup\n";
+print "unlock the tables\ni";
+print "no need for waiting while backup running (it's transaction secure)\n";
+print "is this really what you want?\n";
+print "if so type: im-really-sure, but with out the hypens(-)\n";
+my $safenet=<STDIN>;
+if($safenet=~m/im really sure/){
+	print "ok proceeding, now remember to transfer the database dump to the replica and load it there, then restart replication\n";
+}else{
+	print "ok maybe safest, welcome back though\n";
+	exit;
+}
+
 my $syscheckhome = $ENV{'SYSCHECK_HOME'};
 
 # read mysql root pass from syscheck config
@@ -11,7 +27,6 @@ close SYSCHECKCONF;
 my $mysqlrootpass = undef;
 foreach (@syscheckconf){ 
 	if(m/MYSQLROOT_PASSWORD/gio){
-		print "rootpass:  $_\n";
 		chomp;
 		$_ =~ m/MYSQLROOT_PASSWORD.*=(.*)/;
 		$mysqlrootpass = $1;	
@@ -20,7 +35,6 @@ foreach (@syscheckconf){
 }
 
 
-print "rootpass: $mysqlrootpass\n";
 my $dbh = DBI->connect("DBI:mysql:database=mysql", 'root' , $mysqlrootpass , {'RaiseError' => 1, 'AutoCommit' => 0});
 
 print "RESET MASTER\n";
@@ -57,18 +71,33 @@ if ($@) {
         print "SHOW MASTER STATUS: ok\n";
 }
 
+print localtime(time) . "\n";
+my $pid = fork();
+if (not defined $pid) {
+	print "fork failed";
+} elsif ($pid == 0) {
+    print "backup in process\n";
+    print "backup filename:\n";
+    system("$syscheckhome/related-enabled/904_make_mysql_db_backup.sh --batch");
+    print "to transfer database backup run:\n";
+    print "$syscheckhome/related-enabled/906_ssh-copy-to-remote-machine.sh -s file-from-above replica-hostname /home/jboss jboss /home/jboss/.ssh/id_rsa\n";
+    print "backup done\n";
+    exit 0;
+} else {
 
-my $backup = system("$syscheckhome/related-enabled/904_make_mysql_db_backup.sh -s ");
+	my $sqlunlock="UNLOCK TABLES;";
+	$sth = $dbh->prepare($sqlunlock);
+	$sth->execute;
+	$dbh->commit;
+	if ($@) {
+		die $@; # print the error
+	}else{
+	        print "UNLOCK TABLES: ok\n";
+	}
 
-my $sqlunlock="UNLOCK TABLES;";
-$sth = $dbh->prepare($sqlunlock);
-$sth->execute;
-$dbh->commit;
-if ($@) {
-	die $@; # print the error
-}else{
-        print "UNLOCK TABLES: ok\n";
+	$dbh->disconnect;
 
+	print localtime(time) . "\n";
+	waitpid($pid, 0);
+	print localtime(time) . "\n";
 }
-
-$dbh->disconnect;
