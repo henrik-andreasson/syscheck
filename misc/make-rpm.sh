d3ecc6f768104a2c912dd43b9fd02c1bc22da398
Henrik Andreasson <git@han.pp.se>
2016-08-04 00:19:01 +0200

    fixes #8  create rpm and deb files of syscheck

---
+++ misc/make-rpm.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+NAME="syscheck"
+VERSION="1.8.0"
+DIRECTORIES="--directories /opt/syscheck"
+URL="http://certificateservices.org/project/syscheck"
+VENDOR="Certificate Service from CGI"
+MAINTAINER="Henrik Andreasson <han@certificateservices.se>"
+DESCRIPTION="syscheck is a tool to check services"
+
+while getopts o:v:s: flag; do
+  case $flag in
+    v)
+	VERSION="$OPTARG";
+      ;;
+    ?)
+      exit;
+      ;;
+  esac
+done
+
+
+rm -f *rpm
+
+fpm -s dir -t rpm -n ${NAME} -v ${VERSION} --url "${URL}" --vendor "${VENDOR}" --maintainer "${MAINTAINER}" --description "${DESCRIPTION}" ${DIRECTORIES}  --force  --exclude=".git" --config-files /opt/syscheck/config/ --prefix=/opt/syscheck .
+fpm -s dir -t deb -n ${NAME} -v ${VERSION} --url "${URL}" --vendor "${VENDOR}" --maintainer "${MAINTAINER}" --description "${DESCRIPTION}" ${DIRECTORIES}  --force  --exclude=".git" --config-files /opt/syscheck/config/ --prefix=/opt/syscheck .
+
+#fpm -s dir -t deb -n syscheck -v 1.8  --exclude=".git" --config-files /opt/syscheck/config/ --prefix=/opt/syscheck . 
+
