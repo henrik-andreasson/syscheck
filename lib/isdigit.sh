ca0b121c56469179988631a85adc37ac6a98c80d
henrik-andreasson <git@han.pp.se>
2017-11-30 21:34:46 +0100

    Closes #23 -  change from perl isdigit tool to bash and remove old util isdigit.pl

--- lib/isdigit.sh
+++
@@ -1,7 +0,0 @@
-isdigit(){
-	case $1 in
-    		''|*[!0-9]*) return 1 ;;
-		*) return 0 ;;
-	esac
-
-}
--- lib/proc_checker.sh
+++ lib/proc_checker.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+isdigit(){
+	case $1 in
+    ''|*[!0-9]*) return 1 ;;
+		*)           return 0 ;;
+	esac
+
+}
+
+
 checkpid(){
 	the_pid=$1
 
@@ -10,8 +19,18 @@ checkPidByName(){
 	ps -ef | egrep $the_name  | grep -v grep | grep -v proc_checker.sh
 }
 
-if [ -f $1 ] ; then
-	pid_from_file=`cat $1` 
+proc_checker_help() {
+	echo "$0 <pid-file>|<pid> <procname>"
+	echo "example: $0 /var/run/syslogd.pid syslogd"
+	echo "example: $0 1234 syslogd"
+	exit
+}
+
+if [ "x$1" == "x" ] ; then
+	proc_checker_help
+
+elif [ -f $1 ] ; then
+	pid_from_file=$(cat $1)
 	if [ $? -ne 0 ] ; then
 		echo "FAILED" >&2
 		exit -1
@@ -19,7 +38,8 @@ if [ -f $1 ] ; then
 		checkpid $pid_from_file
 	fi
 
-elif [ `${SYSCHECK_HOME}/lib/isdigit.pl $1 ` ] ; then
+
+elif [ $(isdigit $1) ] ; then
 	checkpid $1
 
 elif [ "x$2" != "x" ] ; then
@@ -27,10 +47,5 @@ elif [ "x$2" != "x" ] ; then
 	checkPidByName $procname
 
 else
-	echo "$0 <pid-file>|<pid> <procname>"
-	echo "example: $0 /var/run/syslogd.pid syslogd"
-	echo "example: $0 1234 syslogd"
-	exit
+	proc_checker_help
 fi
-
-
