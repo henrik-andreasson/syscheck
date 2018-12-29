265f20e80a1579ccbc567c20a55ec0c4b5ec9548
Henrik Andreasson <henrik.andreasson@cgi.com>
2018-12-29 15:53:51 +0100

    use release tool to build and change artifacts dir

--- lib/release.sh
+++ lib/release.sh
@@ -1,39 +1,71 @@
 #!/bin/bash
 
+schelp(){
+
+  /bin/echo
+  /bin/echo -e "sysheck release tool"
+  /bin/echo  "--------------------------------------"
+  /bin/echo "<-p|--program> 'syscheck'"
+	/bin/echo "<-v|--version> '1.0'"
+  /bin/echo
+}
+
+
+# get command line arguments
+INPUTARGS=`/usr/bin/getopt --options "hpvog" --long "help,program:,version:,outpath:,gittag,sign" -- "$@"`
+if [ $? != 0 ] ; then schelp ; fi
+eval set -- "$INPUTARGS"
+
+while true; do
+  case "$1" in
+    -v|--version ) VERSION=$2 ; shift 2;;
+    -p|--program ) PROGRAM=$2 ; shift 2;;
+		-o|--outpath ) OUTPATH=$2 ; shift 2;;
+		-s|--sign    ) SIGN=1 ; shift 2;;
+		-g|--gittag  ) GIT=1 ; shift ;;
+    -h|--help )   schelp;exit;shift;;
+    --) break;;
+  esac
+done
+
+
 orgdir=`pwd`
 
-echo "release no:"
-read rel
-
-echo "progname: (syscheck):"
-read progname
-if [ "x${progname}" = "x" ] ; then
-	progname=syscheck
+if [ "x$VERSION" == "x" ] ; then
+	echo "release no:"
+	read rel
 fi
 
+if [ "x$PROGRAM" == "x" ] ; then
+	echo "progname: (syscheck):"
+	read progname
+fi
 
+if [ "x$OUTPATH" == "x" ] ; then
+	OUTPATH=../releases/
+fi
 
-find . -name \*.sh -exec chmod 755 {} \;
-
-OUTPATH=../releases/
 PROGPATH=${OUTPATH}/${progname}-${rel}
 
 if [ ! -d "${OUTPATH}" ] ; then
     mkdir -p "${OUTPATH}"
 fi
 
-echo "git status locally:"
-git status
-echo "tag in git (Y/n)"
-read tag
-if [ "x$tag" = $"xy" -o "x$tag" = "xY" -o "x$tag" = "x" ] ; then
-        git tag ${progname}-${rel}
-	git push origin "${progname}-${rel}"
+if [ "x$GITTAG" == "x1" ] ; then
+	echo "git status locally:"
+	git status
+	echo "tag in git (Y/n)"
+	read tag
+	if [ "x$tag" = $"xy" -o "x$tag" = "xY" -o "x$tag" = "x" ] ; then
+    git tag ${progname}-${rel}
+		git push origin "${progname}-${rel}"
+	fi
 fi
+
 
 mkdir -p                     ${PROGPATH}/config
 mkdir -p                     ${PROGPATH}/misc
-cp -r ./misc/ 							 ${PROGPATH}/misc/make-rpm-deb.sh
+cp -r ./misc/ 							 ${PROGPATH}/misc/
 cp -r ./config 							 ${PROGPATH}
 cp -r ./console_syscheck.sh  ${PROGPATH}
 cp -r ./doc                  ${PROGPATH}
@@ -49,6 +81,7 @@ cp -r ./scripts-enabled      ${PROGPATH}
 cp -r ./syscheck.sh          ${PROGPATH}
 cp -r ./var                  ${PROGPATH}
 
+#TODO dont use perl
 perl -pi -e "s/SYSCHECK_VERSION=.*/SYSCHECK_VERSION=${rel}/gi"  ${PROGPATH}/config/common.conf
 find ${PROGPATH} -name \*.sh -exec chmod 755 {} \;
 find ${PROGPATH}/scripts-available/ -name \*.sh -exec chmod 755 {} \;
@@ -61,12 +94,16 @@ zipname="${progname}-${rel}.zip"
 cd ${OUTPATH}
 zip --exclude "${progname}-${rel}/.git/*" -r ${zipname} ${progname}-${rel}
 
-md5sum ${zipname}          > ${zipname}.md5
-sha1sum ${zipname}         > ${zipname}.sha1
-gpg -o ${zipname}.gpg -sab   ${zipname}
+if [ "x$SIGN" == "x1" ] ; then
+
+	md5sum ${zipname}          > ${zipname}.md5
+	sha1sum ${zipname}         > ${zipname}.sha1
+	gpg -o ${zipname}.gpg -sab   ${zipname}
+
+fi
 
 cd ${progname}-${rel} ; ./misc/make-rpm-deb.sh -v ${rel}
 
 
 cd $orgdir
-ls -la ${PROGPATH} ${OUTPATH}/${zipname} ${OUTPATH}/${zipname}.md5 ${OUTPATH}/${zipname}.sha1 ${OUTPATH}/${zipname}.gpg
+ls -la  ${OUTPATH}
