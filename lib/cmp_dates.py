d9f4be9a93f2d0ccfca89089e9aa81a5a2fae895
Henrik Andreasson <henrik.andreasson@cgi.com>
2019-03-07 19:01:10 +0100

    Closes #64 - new ocsp check

--- lib/cmp_dates.py
+++ lib/cmp_dates.py
@@ -5,7 +5,7 @@ from datetime import timedelta
 import argparse
 import sys
 
-parser = argparse.ArgumentParser('Util to check CRL validity, compares two date strings and calculate the time left. Calculates also two limits WARN (1/2 of the CRL lifetime), ERROR( 1/4 left of the lifetime). The process returns with these values  0 -> ok,  1 -> warn ,  2 -> error,  3 -> expired.')
+parser = argparse.ArgumentParser('Util to check validity, compares two date strings and calculate the time left. Calculates also two limits WARN (1/2 of the CRL lifetime), ERROR( 1/4 left of the lifetime). The process returns with these values  0 -> ok,  1 -> warn ,  2 -> error,  3 -> expired.')
 parser.add_argument('date1', help='date1 ex: "Mar 18 11:30:38 2017 GMT"')
 parser.add_argument('date2', help='date2 ex: "Mar 18 19:30:38 2017 GMT"')
 parser.add_argument('--warnminutes', type=int, help='set custom level for warning in minutes')
@@ -15,27 +15,27 @@ parser.add_argument('--verbose', action='store_const', const="1")
 args = parser.parse_args()
 
 
-created       = datetime.strptime(args.date1, "%b %d %H:%M:%S %Y %Z")
-expires       = datetime.strptime(args.date2, "%b %d %H:%M:%S %Y %Z")
-now           = datetime.now()
-validity      = expires - created
+created = datetime.strptime(args.date1, "%b %d %H:%M:%S %Y %Z")
+expires = datetime.strptime(args.date2, "%b %d %H:%M:%S %Y %Z")
+now = datetime.now()
+validity = expires - created
 
-if ( args.warnminutes) :
-    warn_time = timedelta(0,args.warnminutes*60)
+if (args.warnminutes):
+    warn_time = timedelta(0, args.warnminutes*60)
 else:
-    warn_time     = validity*1/2
+    warn_time = validity*1/2
 
-if ( args.errorminutes) :
-    error_time = timedelta(0,args.errorminutes*60)
+if (args.errorminutes):
+    error_time = timedelta(0, args.errorminutes*60)
 else:
-    error_time    = validity*1/4
+    error_time = validity*1/4
 
 since_created = now - created
-until_expire  = expires - now
-until_warn    = until_expire - warn_time
-until_error   = until_expire - error_time
+until_expire = expires - now
+until_warn = until_expire - warn_time
+until_error = until_expire - error_time
 
-if ( args.verbose ) :
+if (args.verbose):
     print "date1                :", args.date1
     print "date2                :", args.date2
     print "created              :", created
@@ -51,35 +51,37 @@ if ( args.verbose ) :
     print "time until error     :", until_error
 
 
-if ( args.minutes ) :
+if (args.minutes):
     minutes = int((until_expire.total_seconds() / 60))
 
-if (now > expires) :
-    if ( args.minutes ) :
+# has already expired
+if (now > expires):
+    if (args.minutes):
         print minutes
     else:
-        print "ERROR HAS EXPIRED: ", expires , "(d:h:m:s.mmm)"
+        print "ERROR HAS EXPIRED: ", expires, "(d:h:m:s.mmm)"
     sys.exit(3)
-    
-elif (error_time > until_expire) :
-    if ( args.minutes ) :
+
+# error time has passed
+elif (error_time > until_expire):
+    if (args.minutes):
         print minutes
     else:
         print "ERROR error_time", error_time, "(hh:mm:ss) has passed, time until expiration", until_expire, "(d:h:m:s.mmm)"
     sys.exit(2)
 
-
-elif (warn_time > until_expire) :
-    if ( args.minutes ) :
+# warn time has passed
+elif (warn_time > until_expire):
+    if (args.minutes):
         print minutes
     else:
         print "WARNING warn_time", warn_time, "(hh:mm:ss) has passed, time until error: ", until_error, " time until expiration: ", until_expire, "(hh:mm:ss)"
     sys.exit(1)
 
-else :
-    if ( args.minutes ) :
+# no warn/err all ok
+else:
+    if (args.minutes):
         print minutes
     else:
-        print "expire_in: ", until_expire , "(h:m:s), until_warn: ", until_warn , " until_err: ", until_error ," validity: " , validity
+        print "expire_in: ", until_expire , "(h:m:s), until_warn: ", until_warn , " until_err: ", until_error, " validity: ", validity
     sys.exit(0)
-
---
+++ scripts-available/sc_10_ocsp.sh
@@ -0,0 +1,194 @@
+#!/bin/bash
+
+SYSCHECK_HOME="${SYSCHECK_HOME:-/opt/syscheck}" # use default if  unset
+if [ ! -f ${SYSCHECK_HOME}/syscheck.sh ] ; then
+  echo "Can't find $SYSCHECK_HOME/syscheck.sh"
+  exit
+fi
+
+if [ ! -f ${SYSCHECK_HOME}/syscheck.sh ] ; then echo "Can't find $SYSCHECK_HOME/syscheck.sh" ;exit ; fi
+
+# Import common definitions
+source $SYSCHECK_HOME/config/syscheck-scripts.conf
+
+# script name, used when integrating with nagios/icinga
+SCRIPTNAME=ocsp
+
+# uniq ID of script (please use in the name of this file also for convinice for finding next availavle number)
+SCRIPTID=10
+
+# how many info/warn/error messages
+NO_OF_ERR=6
+initscript $SCRIPTID $NO_OF_ERR
+
+default_script_getopt $*
+
+# main part of script
+
+checkocsp() {
+
+  local OPTIND
+
+  while getopts ":u:i:c:h:a:s:d" opt; do
+    case $opt in
+     u)
+        OCSP_URL="$OPTARG"
+       ;;
+     i)
+        OCSP_ISSUER="$OPTARG"
+       ;;
+     c)
+        OCSP_CERT="$OPTARG"
+       ;;
+     h)
+        OCSP_HOSTNAME="$OPTARG"
+       ;;
+     a)
+        OCSP_CACHAIN="$OPTARG"
+       ;;
+     s)
+        EXPECTED_STATUS="$OPTARG"
+       ;;
+
+     \?)
+       printverbose "Invalid option: -$OPTARG" >&2
+       exit 1
+       ;;
+     :)
+       printverbose "Option -$OPTARG requires an argument." >&2
+       exit 1
+       ;;
+    esac
+  done
+
+  if [ "x$OCSP_ISSUER" = "x" ] ; then printverbose "ISSUER NOT SET" ; exit ; fi
+  if [ "x$OCSP_CACHAIN" = "x" ] ; then printverbose "CHAIN NOT SET" ; exit ; fi
+  if [ "x$OCSP_CERT" = "x" ] ; then printverbose "CERT NOT SET" ; exit ; fi
+  if [ "x$OCSP_HOST" = "x" ] ; then printverbose "HOST NOT SET(is optional)" ; fi
+  if [ "x$OCSP_DEBUG" = "x" ] ; then printverbose "DEBUG NOT SET(is optional)" ; fi
+
+
+  if [ ! -f "$OCSP_CERT"  ] ; then
+    printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $ERROR -e ${ERRNO[1]} -d "${DESCR[1]}" -1 "Can NOT find OCSP_CERT $OCSP_CERT"
+  fi
+
+  if [ ! -f "$OCSP_ISSUER" ] ; then
+    printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $ERROR -e ${ERRNO[1]} -d "${DESCR[1]}" -1 "Can NOT find OCSP_ISSUER $OCSP_ISSUER"
+  fi
+
+	if [ ! -f "$OCSP_CACHAIN" ] ; then
+    printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $ERROR -e ${ERRNO[1]} -d "${DESCR[1]}" -1 "Can NOT find OCSP_CACHAIN $OCSP_CACHAIN"
+  fi
+
+  if [ "x$PRINTVERBOSESCREEN" != "x" ] ; then
+		    CERTINFO=$(openssl x509 -in "$OCSP_CERT" -noout  -subject -issuer -dates 2>&1)
+        CERTINFO_SUBJECT=$(echo "$CERTINFO" | grep "^subject=CN"    | sed 's#^subject=##')
+        CERTINFO_ISSUER=$(echo "$CERTINFO" | grep "^issuer=CN"      | sed 's#^issuer=##')
+        CERTINFO_NOT_BEFORE=$(echo "$CERTINFO" | grep "^notBefore=" | sed 's#^notBefore=##')
+        CERTINFO_NOT_AFTER=$(echo "$CERTINFO" | grep "^notAfter="   | sed 's#^notAfter=##')
+        printverbose "### CERTINFO SUBJECT $CERTINFO_SUBJECT ###"
+        printverbose "### CERTINFO ISSUER  $CERTINFO_ISSUER ####"
+        printverbose "### CERTINFO NOT BEFORE  $CERTINFO_NOT_BEFORE ####"
+        printverbose "### CERTINFO NOT AFTER   $CERTINFO_NOT_AFTER ####"
+        printverbose "openssl ocsp -issuer $OCSP_ISSUER -cert $OCSP_CERT -CAfile $OCSP_CACHAIN -url $OCSP_URL $OCSP_DEBUG "
+    fi
+
+		OCSPINFO=$(openssl ocsp -issuer "$OCSP_ISSUER" -cert "$OCSP_CERT" -CAfile "$OCSP_CACHAIN" -resp_text  -url $OCSP_URL 2>/dev/null)
+#    printverbose "### START OCSP INFO ###"
+#    printverbose $OCSPINFO
+#    printverbose "### END OCSP INFO ###"
+
+    OCSP_STATUS=$(echo "${OCSPINFO}" | egrep "^$OCSP_CERT:" | sed "s#$OCSP_CERT:\ ##" )
+    OCSP_DATE=$(echo "${OCSPINFO}" | egrep "^\ +This Update:" | sed "s#.*This Update: ##" )
+    OCSP_RESPONDER_CERT_ISSUER=$(echo "${OCSPINFO}" | egrep  "^\ +Issuer: " | sed "s#.*Issuer: ##" )
+    OCSP_RESPONDER_CERT_SUBJECT=$(echo "${OCSPINFO}" | egrep  "^\ +Subject: " | sed "s#.*Subject: ##" )
+    OCSP_RESPONDER_CERT_NOT_BEFORE=$(echo "${OCSPINFO}" | egrep  "^\ +Not Before: " | sed "s#.*Not Before: ##" )
+    OCSP_RESPONDER_CERT_NOT_AFTER=$(echo "${OCSPINFO}" | egrep  "^\ +Not After : " | sed "s#.*Not After : ##" )
+    OCSP_RESPONDER_RESPONSE_STATUS=$(echo "${OCSPINFO}" | grep "OCSP Response Status:"  | sed 's#.*OCSP Response Status:##')
+
+    printverbose "### OCSP DATE: $OCSP_DATE ###"
+    printverbose "### OCSP STATUS: $OCSP_STATUS ###"
+    printverbose "### OCSP Responder CERT Subject: $OCSP_RESPONDER_CERT_SUBJECT  ###"
+    printverbose "### OCSP Responder CERT Issuer: $OCSP_RESPONDER_CERT_ISSUER  ###"
+    printverbose "### OCSP Responder CERT Not before: $OCSP_RESPONDER_CERT_NOT_BEFORE  ###"
+    printverbose "### OCSP Responder CERT not after: $OCSP_RESPONDER_CERT_NOT_AFTER  ###"
+    printverbose "### OCSP_RESPONDER_RESPONSE_STATUS: $OCSP_RESPONDER_RESPONSE_STATUS ###"
+
+    if [ "x${OCSP_STATUS}" == "x${EXPECTED_STATUS}" ] ; then
+      printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $INFO -e ${ERRNO[6]} -d "${DESCR[6]}" -1 "${CERTINFO_SUBJECT}: $OCSP_STATUS/$EXPECTED_STATUS)"
+      STATUS=$(expr $STATUS + 1)
+      MESSAGE="${MESSAGE}; ${OCSP_RESPONDER_RESPONSE_STATUS}"
+    fi
+
+    OCSP_RESPONDER_CERT_CHECK_VALIDITY=$(${SYSCHECK_HOME}/lib/cmp_dates.py "$OCSP_RESPONDER_CERT_NOT_BEFORE" "$OCSP_RESPONDER_CERT_NOT_AFTER" --warnminutes "${WARN_OCSP_RESPONDER_CERT_EXPIRE}" --errorminutes "${ERROR_OCSP_RESPONDER_CERT_EXPIRE}" --minutes )
+    OCSP_RESPONDER_CERT_CHECK_RETCODE=$?
+
+    if [ "x$OCSP_RESPONDER_CERT_CHECK_VALIDITY" = "x" ] ; then
+      printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $ERROR -e ${ERRNO[1]} -d "${DESCR[1]}" -1 "$OCSP_RESPONDER_CERT_CHECK_RETCODE"
+      STATUS=$(expr $STATUS + 1)
+      MESSAGE="${MESSAGE}; ${OCSP_RESPONDER_CERT_CHECK_RETCODE}"
+
+# ocsp responder cert has already expired
+    elif [ $OCSP_RESPONDER_CERT_CHECK_RETCODE -eq 3 ] ; then
+      printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $ERROR -e ${ERRNO[5]} -d "${DESCR[5]}" -1 "${OCSP_RESPONDER_CERT_ISSUER}" -2 "${OCSP_RESPONDER_CERT_CHECK_VALIDITY}"
+      STATUS=$(expr $STATUS + 1)
+      MESSAGE="${MESSAGE}; ${OCSP_RESPONDER_CERT_CHECK_RETCODE}"
+
+# ocsp responder cert has passed  configured error time
+    elif [ $OCSP_RESPONDER_CERT_CHECK_RETCODE -eq 2 ] ; then
+      printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $ERROR -e ${ERRNO[4]} -d "${DESCR[4]}" -1 "${OCSP_RESPONDER_CERT_ISSUER}" -2 "${OCSP_RESPONDER_CERT_CHECK_VALIDITY}"
+      STATUS=$(expr $STATUS + 1)
+      MESSAGE="${MESSAGE}; ${OCSP_RESPONDER_CERT_CHECK_RETCODE}"
+
+# ocsp responder cert has passed  configured warn time
+    elif [ $OCSP_RESPONDER_CERT_CHECK_RETCODE -eq 1 ] ; then
+      printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $WARN -e ${ERRNO[3]} -d "${DESCR[3]}" -1 "${OCSP_RESPONDER_CERT_ISSUER}" -2 "${OCSP_RESPONDER_CERT_CHECK_VALIDITY}"
+      STATUS=$(expr $STATUS + 1)
+      MESSAGE="${MESSAGE}; ${OCSP_RESPONDER_CERT_CHECK_RETCODE}"
+
+# ocsp responder cert is before any warn/error time
+    elif [ $OCSP_RESPONDER_CERT_CHECK_RETCODE -eq 0 ] ; then
+      STATUS=$(expr $STATUS + 1)
+      printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $INFO -e ${ERRNO[2]} -d "${DESCR[2]}" -1 "${OCSP_RESPONDER_CERT_ISSUER}" -2 "${OCSP_RESPONDER_CERT_CHECK_VALIDITY}"
+
+    else
+      printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $ERROR -e ${ERRNO[1]} -d "${DESCR[1]}"
+      STATUS=$(expr $STATUS + 1)
+      MESSAGE="${MESSAGE}; problem calculating validity"
+    fi
+
+
+}
+
+
+# global ERRSTATUS for all crl:s (0 is ok)
+STATUS=0
+MESSAGE=""
+
+
+for (( i = 0 ;  i < ${#OCSP_TEST[@]} ; i++ )) ; do
+    SCRIPTINDEX=$(addOneToIndex $SCRIPTINDEX)
+
+    # if [ "x${OCSP_HOST_NAME[$i]}" = "x" ] ; then
+    #   OCSP_HOST_NAME_OPT="-h "
+    # fi
+
+    for ocsp_cert_to_test in ${OCSP_TEST[$i]}/test_*_cert* ; do
+      test_name=$(basename  "$ocsp_cert_to_test" | cut -f1,2 -d_ )
+      expected_status=$(basename  "$ocsp_cert_to_test" | cut -f4 -d_ | cut -f1 -d\.)
+      issuer_cert="${OCSP_TEST[$i]}/${test_name}_issuer.pem"
+      cacerts="${OCSP_TEST[$i]}/${test_name}_cacerts.pem"
+      printverbose "## Current test: cert: $ocsp_cert_to_test to URL: ${OCSP_URL[$i]} issuer: ${issuer_cert} cacerts: ${cacerts}"
+      checkocsp  -c "${ocsp_cert_to_test}" -u "${OCSP_URL[$i]}" -i "${issuer_cert}" -a "${cacerts}" -s "${expected_status}"
+    done
+done
+
+# send the summary message (00)
+
+# if [ "x${ERRSTATUS}" != "x0" ] ; then
+#     printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $ERROR -e ${ERRNO[2]} -d "${DESCR[9]}" -1 "${GLOBALERRMESSAGE}"
+# elif [ "x${WARNSTATUS}" != "x0" ] ; then
+#     printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $WARN  -e ${ERRNO[2]} -d "${DESCR[9]}" -1 "${GLOBALERRMESSAGE}"
+# else
+#     printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $INFO  -e ${ERRNO[2]} -d "${DESCR[2]}"
+# fi
