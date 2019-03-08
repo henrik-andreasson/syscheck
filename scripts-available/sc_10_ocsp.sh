ac64069a4abab307d2b2249162c6c93ba9cdc333
Henrik Andreasson <henrik.andreasson@cgi.com>
2019-03-08 14:12:23 +0100

    add the check summary

--- scripts-available/sc_10_ocsp.sh
+++ scripts-available/sc_10_ocsp.sh
@@ -18,7 +18,7 @@ SCRIPTNAME=ocsp
 SCRIPTID=10
 
 # how many info/warn/error messages
-NO_OF_ERR=6
+NO_OF_ERR=10
 initscript $SCRIPTID $NO_OF_ERR
 
 default_script_getopt $*
@@ -29,7 +29,7 @@ checkocsp() {
 
   local OPTIND
 
-  while getopts ":u:i:c:h:a:s:d" opt; do
+  while getopts ":u:i:c:h:a:s:x:d" opt; do
     case $opt in
      u)
         OCSP_URL="$OPTARG"
@@ -49,7 +49,9 @@ checkocsp() {
      s)
         EXPECTED_STATUS="$OPTARG"
        ;;
-
+     x)
+        SCRIPTINDEX="$OPTARG"
+         ;;
      \?)
        printverbose "Invalid option: -$OPTARG" >&2
        exit 1
@@ -70,20 +72,23 @@ checkocsp() {
 
   if [ ! -f "$OCSP_CERT"  ] ; then
     printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $ERROR -e ${ERRNO[1]} -d "${DESCR[1]}" -1 "Can NOT find OCSP_CERT $OCSP_CERT"
+    return 1
   fi
 
   if [ ! -f "$OCSP_ISSUER" ] ; then
     printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $ERROR -e ${ERRNO[1]} -d "${DESCR[1]}" -1 "Can NOT find OCSP_ISSUER $OCSP_ISSUER"
+    return 1
   fi
 
 	if [ ! -f "$OCSP_CACHAIN" ] ; then
     printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $ERROR -e ${ERRNO[1]} -d "${DESCR[1]}" -1 "Can NOT find OCSP_CACHAIN $OCSP_CACHAIN"
+    return 1
   fi
 
   if [ "x$PRINTVERBOSESCREEN" != "x" ] ; then
 		    CERTINFO=$(openssl x509 -in "$OCSP_CERT" -noout  -subject -issuer -dates 2>&1)
-        CERTINFO_SUBJECT=$(echo "$CERTINFO" | grep "^subject=CN"    | sed 's#^subject=##')
-        CERTINFO_ISSUER=$(echo "$CERTINFO" | grep "^issuer=CN"      | sed 's#^issuer=##')
+        CERTINFO_SUBJECT=$(echo "$CERTINFO" | grep "^subject="    | sed 's#^subject=##')
+        CERTINFO_ISSUER=$(echo "$CERTINFO" | grep "^issuer="      | sed 's#^issuer=##')
         CERTINFO_NOT_BEFORE=$(echo "$CERTINFO" | grep "^notBefore=" | sed 's#^notBefore=##')
         CERTINFO_NOT_AFTER=$(echo "$CERTINFO" | grep "^notAfter="   | sed 's#^notAfter=##')
         printverbose "### CERTINFO SUBJECT $CERTINFO_SUBJECT ###"
@@ -116,45 +121,46 @@ checkocsp() {
 
     if [ "x${OCSP_STATUS}" == "x${EXPECTED_STATUS}" ] ; then
       printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $INFO -e ${ERRNO[6]} -d "${DESCR[6]}" -1 "${CERTINFO_SUBJECT}: $OCSP_STATUS/$EXPECTED_STATUS)"
-      STATUS=$(expr $STATUS + 1)
-      MESSAGE="${MESSAGE}; ${OCSP_RESPONDER_RESPONSE_STATUS}"
+    else
+      printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $WARN -e ${ERRNO[10]} -d "${DESCR[10]}" -1 "${CERTINFO_SUBJECT}: $OCSP_STATUS/$EXPECTED_STATUS)"
+      WARNSTATUS=$(expr $WARNSTATUS + 1)
+      GLOBALMESSAGE="${GLOBALMESSAGE}; ${OCSP_RESPONDER_CERT_ISSUER}"
     fi
 
-    OCSP_RESPONDER_CERT_CHECK_VALIDITY=$(${SYSCHECK_HOME}/lib/cmp_dates.py "$OCSP_RESPONDER_CERT_NOT_BEFORE" "$OCSP_RESPONDER_CERT_NOT_AFTER" --warnminutes "${WARN_OCSP_RESPONDER_CERT_EXPIRE}" --errorminutes "${ERROR_OCSP_RESPONDER_CERT_EXPIRE}" --minutes )
+    OCSP_RESPONDER_CERT_CHECK_VALIDITY=$(${SYSCHECK_HOME}/lib/cmp_dates.py "$OCSP_RESPONDER_CERT_NOT_BEFORE" "$OCSP_RESPONDER_CERT_NOT_AFTER" --warnminutes "${WARN_OCSP_RESPONDER_CERT_EXPIRE}" --errorminutes "${ERROR_OCSP_RESPONDER_CERT_EXPIRE}" )
     OCSP_RESPONDER_CERT_CHECK_RETCODE=$?
 
     if [ "x$OCSP_RESPONDER_CERT_CHECK_VALIDITY" = "x" ] ; then
       printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $ERROR -e ${ERRNO[1]} -d "${DESCR[1]}" -1 "$OCSP_RESPONDER_CERT_CHECK_RETCODE"
-      STATUS=$(expr $STATUS + 1)
-      MESSAGE="${MESSAGE}; ${OCSP_RESPONDER_CERT_CHECK_RETCODE}"
+      ERRSTATUS=$(expr $ERRSTATUS + 1)
+      GLOBALMESSAGE="${GLOBALMESSAGE}; ${OCSP_RESPONDER_CERT_ISSUER}"
 
 # ocsp responder cert has already expired
     elif [ $OCSP_RESPONDER_CERT_CHECK_RETCODE -eq 3 ] ; then
       printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $ERROR -e ${ERRNO[5]} -d "${DESCR[5]}" -1 "${OCSP_RESPONDER_CERT_ISSUER}" -2 "${OCSP_RESPONDER_CERT_CHECK_VALIDITY}"
-      STATUS=$(expr $STATUS + 1)
-      MESSAGE="${MESSAGE}; ${OCSP_RESPONDER_CERT_CHECK_RETCODE}"
+      ERRSTATUS=$(expr $ERRSTATUS + 1)
+      GLOBALMESSAGE="${GLOBALMESSAGE}; ${OCSP_RESPONDER_CERT_ISSUER}"
 
 # ocsp responder cert has passed  configured error time
     elif [ $OCSP_RESPONDER_CERT_CHECK_RETCODE -eq 2 ] ; then
       printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $ERROR -e ${ERRNO[4]} -d "${DESCR[4]}" -1 "${OCSP_RESPONDER_CERT_ISSUER}" -2 "${OCSP_RESPONDER_CERT_CHECK_VALIDITY}"
-      STATUS=$(expr $STATUS + 1)
-      MESSAGE="${MESSAGE}; ${OCSP_RESPONDER_CERT_CHECK_RETCODE}"
+      ERRSTATUS=$(expr $ERRSTATUS + 1)
+      GLOBALMESSAGE="${GLOBALMESSAGE}; ${OCSP_RESPONDER_CERT_ISSUER}"
 
 # ocsp responder cert has passed  configured warn time
     elif [ $OCSP_RESPONDER_CERT_CHECK_RETCODE -eq 1 ] ; then
       printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $WARN -e ${ERRNO[3]} -d "${DESCR[3]}" -1 "${OCSP_RESPONDER_CERT_ISSUER}" -2 "${OCSP_RESPONDER_CERT_CHECK_VALIDITY}"
-      STATUS=$(expr $STATUS + 1)
-      MESSAGE="${MESSAGE}; ${OCSP_RESPONDER_CERT_CHECK_RETCODE}"
+      WARNSTATUS=$(expr $WARNSTATUS + 1)
+      GLOBALMESSAGE="${GLOBALMESSAGE}; ${OCSP_RESPONDER_CERT_ISSUER}"
 
 # ocsp responder cert is before any warn/error time
     elif [ $OCSP_RESPONDER_CERT_CHECK_RETCODE -eq 0 ] ; then
-      STATUS=$(expr $STATUS + 1)
       printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $INFO -e ${ERRNO[2]} -d "${DESCR[2]}" -1 "${OCSP_RESPONDER_CERT_ISSUER}" -2 "${OCSP_RESPONDER_CERT_CHECK_VALIDITY}"
 
     else
       printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $ERROR -e ${ERRNO[1]} -d "${DESCR[1]}"
-      STATUS=$(expr $STATUS + 1)
-      MESSAGE="${MESSAGE}; problem calculating validity"
+      ERRSTATUS=$(expr $ERRSTATUS + 1)
+      GLOBALMESSAGE="${GLOBALMESSAGE};${OCSP_RESPONDER_CERT_ISSUER}; other problem "
     fi
 
 
@@ -162,33 +168,30 @@ checkocsp() {
 
 
 # global ERRSTATUS for all crl:s (0 is ok)
-STATUS=0
-MESSAGE=""
+ERRSTATUS=0
+WARNSTATUS=0
+GLOBALMESSAGE=""
 
 
 for (( i = 0 ;  i < ${#OCSP_TEST[@]} ; i++ )) ; do
     SCRIPTINDEX=$(addOneToIndex $SCRIPTINDEX)
-
-    # if [ "x${OCSP_HOST_NAME[$i]}" = "x" ] ; then
-    #   OCSP_HOST_NAME_OPT="-h "
-    # fi
 
     for ocsp_cert_to_test in ${OCSP_TEST[$i]}/test_*_cert* ; do
       test_name=$(basename  "$ocsp_cert_to_test" | cut -f1,2 -d_ )
       expected_status=$(basename  "$ocsp_cert_to_test" | cut -f4 -d_ | cut -f1 -d\.)
       issuer_cert="${OCSP_TEST[$i]}/${test_name}_issuer.pem"
       cacerts="${OCSP_TEST[$i]}/${test_name}_cacerts.pem"
-      printverbose "## Current test: cert: $ocsp_cert_to_test to URL: ${OCSP_URL[$i]} issuer: ${issuer_cert} cacerts: ${cacerts}"
-      checkocsp  -c "${ocsp_cert_to_test}" -u "${OCSP_URL[$i]}" -i "${issuer_cert}" -a "${cacerts}" -s "${expected_status}"
+      printverbose "## Current test: cert: $ocsp_cert_to_test to URL: ${OCSP_URL[$i]} issuer: ${issuer_cert} cacerts: ${cacerts} -x ${SCRIPTINDEX}"
+      checkocsp  -c "${ocsp_cert_to_test}" -u "${OCSP_URL[$i]}" -i "${issuer_cert}" -a "${cacerts}" -s "${expected_status}" -x "${SCRIPTINDEX}"
     done
 done
 
-# send the summary message (00)
-
-# if [ "x${ERRSTATUS}" != "x0" ] ; then
-#     printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $ERROR -e ${ERRNO[2]} -d "${DESCR[9]}" -1 "${GLOBALERRMESSAGE}"
-# elif [ "x${WARNSTATUS}" != "x0" ] ; then
-#     printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $WARN  -e ${ERRNO[2]} -d "${DESCR[9]}" -1 "${GLOBALERRMESSAGE}"
-# else
-#     printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $INFO  -e ${ERRNO[2]} -d "${DESCR[2]}"
-# fi
+# send the summary GLOBALMESSAGE (00)
+export SCRIPTINDEX="00"
+if [ "x${ERRSTATUS}" != "x0" ] ; then
+     printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $ERROR -e ${ERRNO[7]} -d "${DESCR[7]}" -1 "${GLOBALMESSAGE}" -2 "${ERRSTATUS}" -3 "${WARNSTATUS}"
+elif [ "x${WARNSTATUS}" != "x0" ] ; then
+     printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $WARN  -e ${ERRNO[8]} -d "${DESCR[8]}" -1 "${GLOBALMESSAGE}" -2 "${ERRSTATUS}" -3 "${WARNSTATUS}"
+else
+     printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $INFO  -e ${ERRNO[9]} -d "${DESCR[9]}"
+fi
