fdd85b6a2fe07b8d94c2dcc844959b57e5cb19f0
Henrik Andreasson <henrik.andreasson@cgi.com>
2020-08-07 17:16:27 +0200

    add restart option to 33 closes #75

--- scripts-available/sc_33_healthchecker.sh
+++ scripts-available/sc_33_healthchecker.sh
@@ -18,7 +18,7 @@ SCRIPTNAME=healthcheck
 SCRIPTID=33
 
 # how many info/warn/error messages
-NO_OF_ERR=4
+NO_OF_ERR=6
 initscript $SCRIPTID $NO_OF_ERR
 
 
@@ -41,6 +41,79 @@ done
 
 # main part of script
 
+restartProcess() {
+
+  	local OPTIND
+
+  	while getopts ":t:p:w:n:x:" opt; do
+    case $opt in
+     t)
+        STARTCMD="$OPTARG"
+       ;;
+     p)
+        STOPCMD="$OPTARG"
+       ;;
+     w)
+        WAITTIME="$OPTARG"
+       ;;
+     n)
+        PROCESSNAME="$OPTARG"
+       ;;
+     x)
+        MAXRESTARTS="$OPTARG"
+       ;;
+     \?)
+       printverbose "Invalid option: -$OPTARG" >&2
+       return 1
+       ;;
+     :)
+       printverbose "Option -$OPTARG requires an argument." >&2
+       return 1
+       ;;
+    esac
+  done
+
+  if [ "x$STARTCMD" = "x" ] ; then printverbose "STARTCMD NOT SET" ; return 1; fi
+  if [ "x$STOPCMD" = "x" ] ; then printverbose "STOPCMD NOT SET" ; return 1 ; fi
+  if [ "x$WAITTIME" = "x" ] ; then printverbose "WAITTIME NOT SET" ; return 1 ; fi
+  if [ "x$PROCESSNAME" = "x" ] ; then printverbose "PROCESSNAME NOT SET" ; return 1 ; fi
+  if [ "x$MAXRESTARTS" = "x" ] ; then printverbose "MAXRESTARTS NOT SET" ; return 1 ; fi
+
+  RESTARTLOG="${SYSCHECK_HOME}/var/auto-restart-${PROCESSNAME}.log"
+  NOW=$(date +"%Y-%m-%d_%H.%M.%S")
+  now_ts=$(date +"%s")
+  restartsin24h=0
+  if [ -f "${RESTARTLOG}" ] ; then
+	while read line ; do
+		let row="$row + 1"
+		row_ts=$(echo $line | cut -f2 -d\;)
+		let timediff="$now_ts - $row_ts"
+		if [ $timediff -lt 86400 ] ; then
+			let restartsin24h="$restartsin24h + 1"
+		fi
+	done < "${RESTARTLOG}"
+  else
+    touch "${RESTARTLOG}"
+  fi
+
+  if [ $restartsin24h -gt $MAXRESTARTS ] ; then
+  	printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $ERROR -e ${ERRNO[5]} -d "${DESCR[5]}" -1 "${PROCESSNAME}" -2 "${restartsin24h}"
+	return 1
+  fi
+
+  printtoscreen "stop command: <${STOPCMD}>"
+  STOP_STATUS=$(eval ${STOPCMD} 2>&1)
+  printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $ERROR -e ${ERRNO[4]} -d "${DESCR[4]}" -1 "${HEALTHCHECK_APP[$i]}" -2 "${STOP_STATUS}" -3 "${STOPCMD}" -4 "${restartsin24h}"
+  sleep "${WAITTIME}"
+  printtoscreen "start command: <${STARTCMD}>"
+  START_STATUS=$(eval ${STARTCMD} 2>&1)
+  printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $ERROR -e ${ERRNO[4]} -d "${DESCR[4]}" -1 "${HEALTHCHECK_APP[$i]}" -2 "${START_STATUS}" -3 "${STARTCMD}" -4 "${restartsin24h}"
+  /bin/echo -e "${NOW};${now_ts};${STOPCMD};${STARTCMD}" >> "${RESTARTLOG}"
+}
+
+if [ "x${MAX_RESTARTS}" == "x" ] ; then
+  printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $ERROR -e ${ERRNO[6]} -d "${DESCR[6]}"
+fi
 
 for (( i = 0 ;  i < ${#HEALTHCHECKURL[@]} ; i++ )) ; do
         SCRIPTINDEX=$(addOneToIndex $SCRIPTINDEX)
@@ -51,39 +124,31 @@ for (( i = 0 ;  i < ${#HEALTHCHECKURL[@]} ; i++ )) ; do
 
 	if [ "x${CHECKTOOL}" = "xwget" ] ; then
 	        STATUS=$(${CHECKTOOL} ${HEALTHCHECKURL[$i]} -T ${TIMEOUT} -t 1 2>/dev/null)
+		FULLSTATUS=$(${CHECKTOOL} ${HEALTHCHECKURL_FULL[$i]} -T ${TIMEOUT} -t 1 2>/dev/null)
 		if [ "x${PRINTFULL}" = "x1" ] ; then
-			FULLSTATUS=$(${CHECKTOOL} ${HEALTHCHECKURL_FULL[$i]} -T ${TIMEOUT} -t 1 2>/dev/null)
 			printtoscreen ${FULLSTATUS}
 		fi
 	elif [ "x${CHECKTOOL}" = "xcurl" ] ; then
 	        STATUS=$(${CHECKTOOL} ${HEALTHCHECKURL[$i]} --connect-timeout ${TIMEOUT} --retry 1 2>/dev/null)
+	        FULLSTATUS=$(${CHECKTOOL} ${HEALTHCHECKURL_FULL[$i]} --connect-timeout ${TIMEOUT} --retry 1 2>/dev/null)
 		if [ "x${PRINTFULL}" = "x1" ] ; then
-	        	FULLSTATUS=$(${CHECKTOOL} ${HEALTHCHECKURL_FULL[$i]} --connect-timeout ${TIMEOUT} --retry 1 2>/dev/null)
 			printtoscreen ${FULLSTATUS}
 		fi
 	else
 	        printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $ERROR -e ${ERRNO[3]} -d "${DESCR[3]}"
 	fi
-
+	FIXED_FULL_STATUS=$(echo "${FULLSTATUS}" | tr -d '\\' | tr -d "'"  | sed 's/%/%%/gi' | tr  '\n' ';')
 	if [ "x$STATUS" != "xALLOK" ] ; then
-		printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $ERROR -e ${ERRNO[2]} -d "${DESCR[2]}" -1 "${HEALTHCHECK_APP[$i]}" -2 "$STATUS"
-
-    if [ "x${STOP_CMD[$i]}" != "x" -a  "x${START_CMD[$i]}" != "x" ] ; then
-        STOP_STATUS=$(${STOP_CMD[$i]} 2>&1)
-        printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $ERROR -e ${ERRNO[4]} -d "${DESCR[4]}" -1 "${HEALTHCHECK_APP[$i]}" -2 "${STOP_STATUS}" -3 "${STOP_CMD[$i]}"
-        sleep "${STOP_START_PAUSE}"
-        START_STATUS=$(${START_CMD[$i]} 2>&1)
-        printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $ERROR -e ${ERRNO[4]} -d "${DESCR[4]}" -1 "${HEALTHCHECK_APP[$i]}" -2 "${START_STATUS}" -3 "${START_CMD[$i]}"
-    fi
+		printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $ERROR -e ${ERRNO[2]} -d "${DESCR[2]}" -1 "${HEALTHCHECK_APP[$i]}" -2 "$FIXED_FULL_STATUS"
+    		if [ "x${STOP_CMD[$i]}" != "x" -a  "x${START_CMD[$i]}" != "x" ] ; then
+			restartProcess -n "${HEALTHCHECK_APP[$i]}" -p "${STOP_CMD[$i]}" -t "${START_CMD[$i]}" -w "${STOP_START_PAUSE}" -x "${MAX_RESTARTS}"
+		fi
 	else
-		printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $INFO  -e ${ERRNO[1]} -d "${DESCR[1]}" -1 "${HEALTHCHECK_APP[$i]}"
+		printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $INFO  -e ${ERRNO[1]} -d "${DESCR[1]}" -1 "${HEALTHCHECK_APP[$i]}" -2 "$FIXED_FULL_STATUS"
 	fi
 
 	if [ "x${PRINTFULL}" = "x1" ] ; then
 		printtoscreen "----"
 	fi
-
-
-
 
 done
