c84033fc6934c1985a49d125e66ef979b9553caa
Henrik Andreasson <henrik.andreasson@cgi.com>
2021-05-11 22:38:21 +0200

    formatting

--- change.sh
+++ change.sh
@@ -6,14 +6,21 @@ cd changes
 
 if [ "x${RELEASE}" != "x" ] ; then
   release_dir="${GO_PIPELINE_NAME}/${RELEASE}"
+  release_no="${RELEASE}"
 else
   release_dir="${GO_PIPELINE_NAME}/${GO_PIPELINE_COUNTER}"
+  release_no="${GO_PIPELINE_COUNTER}"
 fi
 
 mkdir -p "${release_dir}"
 
-cp template/instruction.md "${release_dir}"
-cp template/notes.md "${release_dir}"
+echo "# Release: $release_no" > "${release_dir}/instruction.md"
+echo "Date: $(date) "        >> "${release_dir}/instruction.md"
+cat template/instruction.md  >> "${release_dir}/instruction.md"
+
+echo "# Release: $release_no" > "${release_dir}/notes.md"
+echo "Date: $(date) "        >> "${release_dir}/notes.md"
+cat  template/notes.md       >> "${release_dir}/notes.md"
 
 mkdir -p "${release_dir}/cicd"
 cp ../reports/* "${release_dir}/cicd"
