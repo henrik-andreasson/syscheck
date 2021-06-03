6284a3bc8609a506f7b6c2f88e7314212433c200
Henrik Andreasson <henrik.andreasson@cgi.com>
2021-06-03 22:40:15 +0200

    change id to 44 and remove dependency on new openssl  new script to check https server certificates - updates #102

--- scripts-available/sc_44_cert_from_webserver.sh
+++ scripts-available/sc_44_cert_from_webserver.sh
@@ -15,7 +15,7 @@ source $SYSCHECK_HOME/config/syscheck-scripts.conf
 SCRIPTNAME=cert_from_webserver
 
 # uniq ID of script (please use in the name of this file also for convinice for finding next availavle number)
-SCRIPTID=43
+SCRIPTID=44
 
 # how many info/warn/error messages
 NO_OF_ERR=5
@@ -34,7 +34,7 @@ checkhttpscert () {
 	while true; do
 	  case "$1" in
 	    -s|--servicename     ) SERVICENAME=$2   ; shift 2 ;;
-      -p|--portno          ) PORTNO=$2        ; shift 2 ;;
+            -p|--portno          ) PORTNO=$2        ; shift 2 ;;
 	    -w|--warn            ) WARNDAYS=$2      ; shift 2 ;;
 	    -e|--err             ) ERRDAYS=$2       ; shift 2 ;;
 	    -i|--ip              ) HOST_IP=$2       ; shift 2 ;;
@@ -50,28 +50,27 @@ checkhttpscert () {
   fi
 
   if [ "x$WARNDAYS" != "xdefault" ] ; then
-    let WARNSEC="$WARNDAYS * 86400"
-    ARGWARN="-checkend=$WARNSEC"
+    let WARNMIN="$WARNDAYS * 1440"
   fi
 
   if [ "x$ERRDAYS" != "xdefault" ] ; then
-    let ERRSEC="$ERRDAYS * 86400"
-    ARGERR="-checkend=$ERRSEC"
+    let ERRMIN="$ERRDAYS * 1440"
   fi
 
-  if [ "x$HOST_IP" != "x" ] ; then
-    ARGCONNECT="-connect $HOST_IP:$PORTNO -servername $SERVICENAME"
-  else
+  if [ "x$HOST_IP" == "xdefault" ] ; then
     ARGCONNECT="-connect $SERVICENAME:$PORTNO"
+  else
+    ARGCONNECT="-connect $HOST_IP:$PORTNO -servername $SERVICENAME"
   fi
 
   cd /tmp
   outname=$(mktemp)
   echo "" | openssl s_client $ARGCONNECT  >> $outname 2>&1
-  if [ $? -ne 0 ] ; then
-      printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $ERROR -e ${ERRNO[3]} -d "${DESCR[3]}" -1 "$CRLNAME"
+  check_outname=$(cat "$outname" | grep "Server certificate")
+  if [ "x$outname" == "x" ] ; then
+      printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $ERROR -e ${ERRNO[7]} -d "${DESCR[7]}" -1 "${SERVICENAME}" -2 "${PORTNO}" -3 "${HOST_IP}" -4 "${ERRDAYS}" 
       ERRSTATUS=$(expr $ERRSTATUS + 1)
-      GLOBALERRMESSAGE="${GLOBALERRMESSAGE};${DESCR[3]} $CRLNAME"
+      GLOBALERRMESSAGE="${GLOBALERRMESSAGE};${DESCR[7]}"
       return 1
   fi
 
@@ -102,23 +101,35 @@ checkhttpscert () {
 
   issuer=$(openssl x509 -in "$certfile" -issuer -noout | sed 's/issuer=//')
   subject=$(openssl x509 -in "$certfile" -issuer -noout | sed 's/subject=//')
-
-  warnhaspassed=$(openssl x509 -in "$certfile" ${ARGWARN}  -noout | sed 's/notAfter//')
-  errhaspassed=$(openssl x509 -in "$certfile" ${ARGERR} -noout | sed 's/notBefore//')
   certexpiredate=$(openssl x509 -in "$certfile" -enddate  -noout | sed 's/notAfter=//')
 
-  if [ "x$errhaspassed" == "xCertificate will expire" ] ; then
-    printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $ERROR -e ${ERRNO[3]} -d "${DESCR[3]}" -1 "${SERVICENAME}" -2 "${PORTNO}" -3 "${HOST_IP}" -4 "${ERRDAYS}"  -5 "${certexpiredate}"
-    ERRSTATUS=$(expr $ERRSTATUS + 1)
-    GLOBALERRMESSAGE="${GLOBALERRMESSAGE};ERR ${SERVICENAME} ${PORTNO} ${HOST_IP}"
+  expireinfo=$($SYSCHECK_HOME/lib/cmp_dates.py --warnminutes="${WARNMIN}" --errorminutes="${ERRMIN}"  "$(date +'%b %d %H:%M:%S %Y %Z')" "${certexpiredate}")
+  retcode=$?
+  if [ $retcode -eq 0 ] ; then
+    printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $INFO -e ${ERRNO[1]} -d "${DESCR[1]}" -1 "${SERVICENAME}" -2 "${PORTNO}" -3 "${HOST_IP}"
 
-  elif [ "x$warnhaspassed" == "xCertificate will expire" ] ; then
+  elif [ $retcode -eq 1 ] ; then
     printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $WARN -e ${ERRNO[2]} -d "${DESCR[2]}" -1 "${SERVICENAME}" -2 "${PORTNO}" -3 "${HOST_IP}" -4 "${WARNDAYS}" -5 "${certexpiredate}"
     WARNSTATUS=$(expr $WARNSTATUS + 1)
     GLOBALERRMESSAGE="${GLOBALWARNESSAGE};WARN ${SERVICENAME} ${PORTNO} ${HOST_IP}"
 
+  elif [ $retcode -eq 2 ] ; then
+    printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $ERROR -e ${ERRNO[3]} -d "${DESCR[3]}" -1 "${SERVICENAME}" -2 "${PORTNO}" -3 "${HOST_IP}" -4 "${ERRDAYS}"  -5 "${certexpiredate}"
+    ERRSTATUS=$(expr $ERRSTATUS + 1)
+    GLOBALERRMESSAGE="${GLOBALERRMESSAGE};ERR ${SERVICENAME} ${PORTNO} ${HOST_IP}"
+
+  elif [ $retcode -eq 3 ] ; then
+    printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $ERROR -e ${ERRNO[8]} -d "${DESCR[8]}" -1 "${SERVICENAME}" -2 "${PORTNO}" -3 "${HOST_IP}" -4 "${ERRDAYS}"  -5 "${certexpiredate}"
+    ERRSTATUS=$(expr $ERRSTATUS + 1)
+    GLOBALERRMESSAGE="${GLOBALERRMESSAGE};ERR ${SERVICENAME} ${PORTNO} ${HOST_IP}"
+
   else
-    printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $INFO -e ${ERRNO[1]} -d "${DESCR[1]}" -1 "${SERVICENAME}" -2 "${PORTNO}" -3 "${HOST_IP}"
+
+    #TODO: case should  not happen not happen not happen
+    printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $ERROR -e ${ERRNO[8]} -d "${DESCR[8]}" -1 "${SERVICENAME}" -2 "${PORTNO}" -3 "${HOST_IP}" -4 "${ERRDAYS}"  -5 "${certexpiredate}"
+    ERRSTATUS=$(expr $ERRSTATUS + 1)
+    GLOBALERRMESSAGE="${GLOBALERRMESSAGE};ERR ${SERVICENAME} ${PORTNO} ${HOST_IP}"
+
   fi
   rm "$outname"
   rm "$certfile"
@@ -149,7 +160,7 @@ for (( i = 0 ;  i < ${#SERVICENAME[@]} ; i++ )) ; do
       HOST_IP="default"
     fi
 
-    checkhttpscert --servicename "${SERVICENAME[$i]}" -p "${PORTNO[$i]}"  -w "${WARNTIME}" -e "${ERRTIME}"
+    checkhttpscert --servicename "${SERVICENAME[$i]}" -p "${PORTNO[$i]}" -i "${HOST_IP}" -w "${WARNTIME}" -e "${ERRTIME}"
 
 done
 
