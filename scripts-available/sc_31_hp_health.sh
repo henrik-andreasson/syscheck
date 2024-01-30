3e4deb8997f29b56327393272e45431f7341e22c
linusm <linus.moller@cgi.com>
2024-01-30 13:43:02 +0100

    updates for HP-healthcheck

--- scripts-available/sc_31_hp_health.sh
+++ scripts-available/sc_31_hp_health.sh
@@ -27,17 +27,20 @@ default_script_getopt $*
 
 
 hppsu () {
-        COMMAND=`/bin/echo -e "show powersupply\nexit" | /sbin/hpasmcli | egrep "Present|Condition"|cut -f2 -d":"|awk '{printf "%s;",$1}'`
-        STATUSPSU1=`echo $COMMAND | cut -f1 -d\; |grep -i "yes"`
-        CONDPSU1=`echo $COMMAND | cut -f2 -d\; |grep -i "ok"`
-        STATUSPSU2=`echo $COMMAND | cut -f3 -d\; |grep -i "yes"`
-        CONDPSU2=`echo $COMMAND | cut -f4 -d\; |grep -i "ok"`
+        COMMAND=$($HP_HEALTH_TOOL serverinfo --power | grep -E "State|Health" | cut -f2 -d":" | awk '{printf "%s;",$1}')
+        STATUSPSU1=`echo $COMMAND | cut -f1 -d\; | grep -i "ok"`
+        CONDPSU1=`echo $COMMAND | cut -f2 -d\; | grep -i "enabled"`
+        STATUSPSU2=`echo $COMMAND | cut -f3 -d\; | grep -i "ok"`
+        CONDPSU2=`echo $COMMAND | cut -f4 -d\; | grep -i "enabled"`
+        STATUSPSU3=`echo $COMMAND | cut -f5 -d\; | grep -i "ok"`
+        CONDPSU3=`echo $COMMAND | cut -f6 -d\; | grep -i "enabled"`
 
 	SCRIPTINDEX=$(addOneToIndex $SCRIPTINDEX)
         if [ "x$STATUSPSU1" != "x" -a "x$CONDPSU1" != "x" ] ; then
                 printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $INFO -e ${ERRNO[1]} -d "${DESCR[1]}" -1 "PSU1"
         else
                 printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $ERROR -e ${ERRNO[2]} -d "${DESCR[2]}" -1 "PSU1 $STATUSPSU1 $CONDPSU1"
+		ERRSTATUS=1
         fi
 
 	SCRIPTINDEX=$(addOneToIndex $SCRIPTINDEX)
@@ -45,60 +48,89 @@ hppsu () {
                 printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $INFO  -e ${ERRNO[1]} -d "${DESCR[1]}" -1 "PSU2"
         else
                 printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $ERROR -e ${ERRNO[2]} -d "${DESCR[2]}" -1 "PSU2 $STATUSPSU2 $CONDPSU2"
+		ERRSTATUS=1
+        fi
+
+	SCRIPTINDEX=$(addOneToIndex $SCRIPTINDEX)
+        if [ "x$STATUSPSU3" != "x" -a "x$CONDPSU3" != "x" ] ; then
+                printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $INFO  -e ${ERRNO[1]} -d "${DESCR[1]}" -1 "PSU Group"
+        else
+                printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $ERROR -e ${ERRNO[2]} -d "${DESCR[2]}" -1 "PSU Group $STATUSPSU3 $CONDPSU3"
+		ERRSTATUS=1
         fi
 }
 
 hptemp () {
-  for tempinput in $(/bin/echo -e "show temp\nexit" | /sbin/hpasmcli | grep '^#' |awk '{print $1,$2,$3,$4}'|sed 's/ /;/g') ; do
-    ##15;I/O_ZONE;33C/91F;70C/158F
-    TEMPNO=`echo $tempinput | cut -f1 -d\;`
-    TEMPNAME=`echo $tempinput | cut -f2 -d\;`
-    TEMPVAL=`echo $tempinput | cut -f3 -d\; |sed 's,C/.*,,'`
-    TEMPLIMIT=`echo $tempinput | cut -f4 -d\; |sed 's,C/.*,,'`
-    SCRIPTINDEX=$(addOneToIndex $SCRIPTINDEX)
+
+ parse_temp () {
+  {
+	$HP_HEALTH_TOOL serverinfo --thermals | grep -Evi '(^-|restful|copyright)' | grep -E "Location|Current|Critical|Health" | cut -f2 -d":" | awk '{print $1}'
+  } | 
+ (set -f ; unset IFS ; set -- $(cat)
+ while [ $# -ge 4 ] ; do {
+	printf '%s;%s;%s;%s\n' "$1" "$2" "$3" "$4"
+	shift 4 ; } done
+ printf '%s' "$@")
+ }
+
+    for TEMPINPUT in $(parse_temp) ; do
+      TEMPNAME=$(echo $TEMPINPUT | cut -f1 -d\;)
+      TEMPVAL=$(echo $TEMPINPUT | cut -f2 -d\;)
+      TEMPLIMIT=$(echo $TEMPINPUT | cut -f3 -d\; |sed 's,C/.*,,')
+      TEMPHEALTH=$(echo $TEMPINPUT | cut -f4 -d\; |sed 's,C/.*,,')
+      SCRIPTINDEX=$(addOneToIndex $SCRIPTINDEX)
 
     echo ${HPTEMP}|egrep -q "${TEMPNO} "
-    if [ $? != 0 ];then
-      printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX}  -l $INFO -e ${ERRNO[1]} -d "${DESCR[1]}" -1 "TEMP ${TEMPNO} ${TEMPNAME} is known not to give any reading ($tempinput)"
+    if [[ "x${TEMPLIMIT}" = @(xNone|x-) ]];then
+      printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX}  -l $INFO -e ${ERRNO[1]} -d "${DESCR[1]}" -1 "TEMP ${TEMPNO} ${TEMPNAME} is known not to give any threshold ($TEMPINPUT)"
       #continue
     else
       if [ "x${TEMPVAL}" = "x" ] ; then
-        printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $ERROR -e ${ERRNO[2]} -d "${DESCR[2]}" -1 "TEMP  ${TEMPNO} ${TEMPNAME} did not return any value for CURRENT temp ($tempinput)"
+        printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $ERROR -e ${ERRNO[2]} -d "${DESCR[2]}" -1 "TEMP  ${TEMPNO} ${TEMPNAME} did not return any value for CURRENT temp ($TEMPINPUT)"
         continue
       fi
       if [ "x${TEMPLIMIT}" = "x" ] ; then
-        printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $ERROR -e ${ERRNO[2]} -d "${DESCR[2]}" -1 "TEMP  ${TEMPNO} ${TEMPNAME} did not return any value for LIMIT temp ($tempinput)"
+        printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $ERROR -e ${ERRNO[2]} -d "${DESCR[2]}" -1 "TEMP  ${TEMPNO} ${TEMPNAME} did not return any value for LIMIT temp ($TEMPINPUT)"
         continue
       fi
       if [ ${TEMPVAL} -gt ${TEMPLIMIT} ] ; then
         printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $ERROR -e ${ERRNO[2]} -d "${DESCR[2]}" -1 "TEMP ${TEMPNO} ${TEMPNAME} Current: ${TEMPVAL} Limit: ${TEMPLIMIT} (celsius)"
+	ERRSTATUS=1
       else
         printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $INFO  -e ${ERRNO[1]} -d "${DESCR[1]}" -1 "TEMP ${TEMPNO} ${TEMPNAME} Current: ${TEMPVAL} Limit: ${TEMPLIMIT} (celsius)"
       fi
     fi
   done
 }
+
 hpfans () {
-  for fansinput in $(/bin/echo -e "show fans\nexit" | /sbin/hpasmcli | grep '^#' |awk '{print $1,$2,$3,$4,$5}'|sed 's/ /;/g') ; do
-    #Fan  Location        Present Speed  of max  Redundant  Partner  Hot-pluggable
-    ##1;SYSTEM;Yes;NORMAL;29%;Yes;0;Yes;
-    FANNO=`echo $fansinput | cut -f1 -d\;`
-    FANLOC=`echo $fansinput | cut -f2 -d\;`
-    FANPRESENT=`echo $fansinput | cut -f3 -d\;`
-    FANSPEED=`echo $fansinput | cut -f4 -d\; `
-    FANPERCENT=`echo $fansinput | cut -f5 -d\;|sed 's/%//' `
-    SCRIPTINDEX=$(addOneToIndex $SCRIPTINDEX)
-    if [ $FANPRESENT = "No" ];then
-      printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX}  -l $INFO -e ${ERRNO[1]} -d "${DESCR[1]}" -1 "FAN ${FANNO} ${FANLOC} is not installed "
-    else
-      if [ "x${FANSPEED}" !=  "xNORMAL" ] ; then
-        printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $ERROR -e ${ERRNO[3]} -d "${DESCR[3]}" -1 "FAN(${FANNO}) NOT in normal operation (${FANSPEED}/${FANPERCENT}%%)"
+
+ parse_fans () {
+  {
+	$HP_HEALTH_TOOL serverinfo --fans | grep -Evi '(^-|restful|copyright)' | grep -E "Reading|Health" | cut -f2 -d":" | awk '{print $1}'
+  } | 
+ (set -f ; unset IFS ; set -- $(cat)
+ while [ $# -ge 2 ] ; do {
+	printf '%s;%s\n' "$1" "$2"
+	shift 2 ; } done
+ printf '%s' "$@")
+ }
+
+    for FANSINPUT in $(parse_fans) ; do
+
+      FANPERCENT=$(echo $FANSINPUT | cut -f1 -d\;)
+      FANSTATUS=$(echo $FANSINPUT | cut -f2 -d\;)
+      SCRIPTINDEX=$(addOneToIndex $SCRIPTINDEX)
+
+      if [ "x${FANSTATUS}" !=  "xOK" ] ; then
+        printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $ERROR -e ${ERRNO[3]} -d "${DESCR[3]}" -1 "FAN(${FANSTATUS}) NOT in normal operation ${FANPERCENT}%" 
+	ERRSTATUS=1
       else
-        printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $INFO  -e ${ERRNO[1]} -d "${DESCR[1]}" -1 "FAN(${FANNO}) IS in normal operation (${FANSPEED}/${FANPERCENT}%%)"
+        printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $INFO  -e ${ERRNO[1]} -d "${DESCR[1]}" -1 "FAN is ${FANSTATUS} in normal operation ${FANPERCENT}%"
       fi
-    fi
-  done
+    done
 }
+
 if [ ! -x $HP_HEALTH_TOOL ] ; then
     printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $ERROR -e ${ERRNO[4]} -d "${DESCR[4]}" -1 "$HP_HEALTH_TOOL"
     exit
