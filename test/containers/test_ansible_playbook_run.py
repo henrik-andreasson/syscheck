ec924ab814ae618d942b375821e9a1dcdb798534
Henrik Andreasson <henrik.andreasson@gmail.com>
2026-09-18 13:41:27 +0000

    test(ansible): update playbook execution test to use henrikandreasson collection namespace

--- test/containers/test_ansible_playbook_run.py
+++ test/containers/test_ansible_playbook_run.py
@@ -88,7 +88,7 @@ def test_ansible_playbook_run_and_installation():
 
     # Set up temporary ANSIBLE_COLLECTIONS_PATH structure
     collections_dir = "/tmp/ansible_test_collections_run"
-    collection_link_dir = os.path.join(collections_dir, "ansible_collections/aberosecurity")
+    collection_link_dir = os.path.join(collections_dir, "ansible_collections/henrikandreasson")
     os.makedirs(collection_link_dir, exist_ok=True)
     
     # Symlink our collection root (misc/ansible) to the collection path
