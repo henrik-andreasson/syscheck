a96f4f423ae5896e5434cb144adeede80729f821
Henrik Andreasson <henrik.andreasson@gmail.com>
2026-09-18 08:21:12 +0000

    fix(929): make filter repeatable, improve real-data regex and fix false-failure report

--- related-available/929_filter_syscheck_messages.sh
+++ related-available/929_filter_syscheck_messages.sh
@@ -16,7 +16,7 @@ SCRIPTNAME=filter_syscheck_messages
 SCRIPTID=929
 
 # how many info/warn/error messages
-NO_OF_ERR=2
+NO_OF_ERR=3
 initscript $SCRIPTID $NO_OF_ERR
 
 # get command line arguments
@@ -41,11 +41,20 @@ SEND_ONLY_SCRIPT_IDS_FMT=$(echo ${SEND_ONLY_SCRIPT_IDS} | sed 's/ /|/g')
 
 SCRIPTINDEX=$(addOneToIndex $SCRIPTINDEX)
 
-egrep "(${SEND_ONLY_SCRIPT_IDS_FMT})-" ${LOCAL_FILE}  > ${FILTERED_FILE}
+# build beside the real file so a failed run leaves the last good one in place
+# mktemp creates the file, so the redirect needs >| to get past noclobber
+FILTERED_TMP=$(mktemp "${FILTERED_FILE}.XXXXXX")
+egrep "^(${SEND_ONLY_SCRIPT_IDS_FMT})-" "${LOCAL_FILE}" >| "${FILTERED_TMP}"
+EGREPRET=$?
 
-
-if [ $? = 0 ] ; then
+# egrep returns 1 when nothing matched, which is a quiet period, not a failure
+if [ "$EGREPRET" -eq 0 ] ; then
+      mv -f "${FILTERED_TMP}" "${FILTERED_FILE}"
       printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $INFO -e ${ERRNO[1]} -d "${DESCR[1]}"
+elif [ "$EGREPRET" -eq 1 ] ; then
+      mv -f "${FILTERED_TMP}" "${FILTERED_FILE}"
+      printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $INFO -e ${ERRNO[3]} -d "${DESCR[3]}" -1 "${SEND_ONLY_SCRIPT_IDS}"
 else
+      rm -f "${FILTERED_TMP}"
       printlogmess -n ${SCRIPTNAME} -i ${SCRIPTID} -x ${SCRIPTINDEX} -l $ERROR -e ${ERRNO[2]} -d "${DESCR[2]}"
 fi
